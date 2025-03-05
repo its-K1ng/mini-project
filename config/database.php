85d39fab3867a32cc6e167857c15a3f683900c68
crlt-King <yang.danyang.6.29@gmail.com>
2025-03-05 23:28:31 +0700

    finalisasi

--- config/database.php
+++ config/database.php
@@ -1,22 +1,29 @@
 <?php
 class Database {
-    private $host = 'localhost';
-    private $db_name = 'tabungan_db';
-    private $username = 'root';
-    private $password = '';
-    private $conn;
+     // Properti untuk menyimpan konfigurasi koneksi ke database
+    private $host = 'localhost'; // host database 
+    private $db_name = 'tabungan_db'; //nama database
+    private $username = 'root'; // username untuk mengakses database (default: 'root' untuk XAMPP)
+    private $password = ''; // password untuk mengakses database (default kosong untuk XAMPP)
+    private $conn; //properti untuk menyimpan koneksi ke database
 
+    // Metode untuk membuat koneksi ke database
     public function connect() {
         try {
+            // membuat koneksi PDO ke database menggunakan konfigurasi yang telah ditentukan
             $this->conn = new PDO(
                 "mysql:host={$this->host};dbname={$this->db_name}",
                 $this->username,
                 $this->password
             );
+            // mengatur mode error pada koneksi PDO agar menampilkan exception jika terjadi kesalahan
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            // mengembalikan objek koneksi PDO jika berhasil
             return $this->conn;
         } catch(PDOException $e) {
+            // Menangkap dan menampilkan pesan kesalahan jika koneksi gagal
             echo "Connection Error: " . $e->getMessage();
+            // Mengembalikan null sebagai tanda kegagalan koneksi
             return null;
         }
     }
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,14 @@
 <?php
 class Router {
+    // metode yang bertugas untuk memetakan URL ke kontroler yang sesuai
     public function route($url) {
+        // struktur switch untuk memeriksa URL yang diterima
+
         switch($url) {
-            case 'home':
-                require_once 'app/controllers/HomeController.php';
-                $controller = new HomeController();
-                $controller->index();
+            case 'home': // jika URL adalah 'home'
+                require_once 'app/controllers/HomeController.php'; // memuat file HomeController
+                $controller = new HomeController(); // membuat instance dari HomeController
+                $controller->index(); // memanggil metode 'index' pada HomeController
                 break;
             
             case 'admin':
