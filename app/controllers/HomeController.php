194ede4f0025baa44d5c3e63298ec4e0d87ea106
crlt-King <yang.danyang.6.29@gmail.com>
2025-03-05 13:18:23 +0700

    Authmiddleware

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -16,13 +16,7 @@ class HomeController {
     
         $userId = AuthMiddleware::getUserId();
         $isAdmin = $_SESSION['user_role'] === 'admin';
-    
-        if ($isAdmin) {
-            $savings = $this->savingModel->getAll(); // Admin melihat semua data
-        } else {
-            $savings = $this->savingModel->getByUserId($userId); // User hanya melihat miliknya
-        }
-    
+        $savings = $this->savingModel->getByUserId($userId); // Admin & user hanya lihat setorannya sendiri
         require_once 'app/views/home.php';
     }
     
